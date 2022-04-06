84099cdf56d5176c6d46bae07706458967c654b8
yukaidesu <yukai_tsutsumi@pj100.biz>
2022-04-06 17:37:05 +0900

    LoginPage Commit

---
+++ src/pages/login.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+
+export default function FirstPost() {
+  return (
+    <div>
+
+        <main>
+            <h1>履歴</h1>
+        </main>
+
+        <footer>
+
+        </footer>
+    </div>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  future: {},
+  purge: ['./src/**/*.{js,ts,jsx,tsx'],
+  theme: {
+    extend: {
+      colors: {
+        'gray': '#f7f7f9',
+        'red': '#fa5b55',
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
