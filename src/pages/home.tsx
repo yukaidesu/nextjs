b71f864b0e0edb6e605c8b024fb8cdda584cf324
yukaidesu <yukai_tsutsumi@pj100.biz>
2022-04-07 11:17:03 +0900

    RemakeLoginPage

---
+++ src/pages/home.tsx
@@ -0,0 +1,14 @@
+export default function FirstPost() {
+  return (
+    <div>
+
+        <main>
+            <h1>履歴</h1>
+        </main>
+
+        <footer>
+
+        </footer>
+    </div>
+  )
+}
